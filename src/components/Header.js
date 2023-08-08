3dbaef3845d5191c72f46abcc8bd7ebba8f671f8
usorfaitheloho <usorfaitheloho@gmail.com>
2023-08-08 12:13:40 +0100

    Update Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import logo from '../images/logo.png';
 
-const Header = () => (
-  <div />
-);
 
-export default Header;
+
+const Header = () => {
+  return (
+    <Header>
+        <div className=''>
+
+        </div>
+    </Header>
+    
+  )
+}
+
+export default Header
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+const Navbar = () => {
+  const nav = [
+    {
+      id: 1,
+      path: '/',
+      children: 'Home',
+    },
+    {
+      id: 2,
+      path: '/Shop',
+      children: 'Buy',
+    },
+    {
+      id: 3,
+      path: '/about',
+      children: 'About Us',
+    },
+    {
+      id: 2,
+      path: '/Contact Us',
+      children: 'Contact Us',
+    },
+  ];
+
+  const navigation = nav.map(({ id, path, children }) => (
+
+    <li key={id}>
+      <Link
+        to={path}
+        end
+      >
+        {children}
+      </Link>
+    </li>
+
+  ));
+
+  return (
+    <nav>
+      <ul>
+        {navigation}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
