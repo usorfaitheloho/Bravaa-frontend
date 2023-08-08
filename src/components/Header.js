aa9c08a1d7f14553c02aace4bac7dcf10df6d7ee
usorfaitheloho <usorfaitheloho@gmail.com>
2023-08-08 12:19:55 +0100

    Update Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import MediaQuery from 'react-responsive';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import logo from '../images/logo.png';
 
+const Header = () => (
+  <Header>
+    <div className="" />
+  </Header>
 
+);
 
-const Header = () => {
-  return (
-    <Header>
-        <div className=''>
-
-        </div>
-    </Header>
-    
-  )
-}
-
-export default Header
+export default Header;
