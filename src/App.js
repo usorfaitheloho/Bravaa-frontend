ca26bd900964dbde99964fc90257d34ffd798bad
usorfaitheloho <usorfaitheloho@gmail.com>
2023-12-29 21:19:26 +0200

    Add search link to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
+import Search from './components/Search';
 
 const App = () => (
   <>
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/header" element={<Header />} />
+      <Route exact path="/search" element={<Search />} />
     </Routes>
   </>
 );
