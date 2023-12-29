4359a73e40a778ef004348b11bb8635462ea4a7f
usorfaitheloho <usorfaitheloho@gmail.com>
2023-12-29 21:20:02 +0200

    Impleent search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,17 @@
 import React from 'react';
-// import logo from '../images/logo.png';
+import search from '../images/search.png';
 
 const Search = () => (
-  <div>Search</div>
-  //  <img src={logo} alt="Logo" className="h-[15px] md:h-[20px] lg:h-[30px]" />
-
+  <div className="relative flex items-center">
+    <input
+      type="text"
+      placeholder="Searching..."
+      className="w-56 p-1 pl-8 placeholder-green-700 border border-gray-300 rounded-full pl pl- pl-8pr-12 pr- w- focus:outline-none focus:border-blue-500 lg:w-2/5 placeholder:text-sm"
+    />
+    <div className="absolute text-gray-500 left-2 top-2">
+      <img src={search} className="md:h-[20px] p-1" alt="search" />
+    </div>
+  </div>
 );
 
 export default Search;
