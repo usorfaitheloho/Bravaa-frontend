c496f7ecb79b01c2b2f0831df2e603e64e578047
usorfaitheloho <usorfaitheloho@gmail.com>
2023-12-29 22:37:27 +0200

    Add search and menu icon to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '../images/logo.png';
+import Search from './Search';
+import menu from '../images/menu.png';
 
 const Header = () => (
   <header>
     <div className="flex items-center justify-between h-16 p-4">
       <img src={logo} alt="Logo" className="h-[12px] md:h-[20px] lg:h-[30px] w-14" />
-      <h1> This is the header page </h1>
+      <Search />
+      <img src={menu} alt="menu" className="w-8 p-2 border rounded-full" />
     </div>
   </header>
 
