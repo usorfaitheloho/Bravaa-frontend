e5f3e9149ef9f99071075a72eaea8f682948658e
usorfaitheloho <usorfaitheloho@gmail.com>
2023-12-29 17:51:10 +0200

    Replace the olld logo with the new logo

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,10 @@
 import React from 'react';
+// import logo from '../images/logo.png';
 
 const Search = () => (
   <div>Search</div>
+  //  <img src={logo} alt="Logo" className="h-[15px] md:h-[20px] lg:h-[30px]" />
+
 );
 
 export default Search;
