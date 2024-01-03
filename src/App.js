87444f5d71feb69ae4b5745dc53713697bb8ccd4
usorfaitheloho <usorfaitheloho@gmail.com>
2024-01-04 00:12:44 +0200

    Update Header component to navigation component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Header from './components/Header';
 import Search from './components/Search';
+import Navigation from './components/Navigation';
 
 const App = () => (
   <>
     <Routes>
       <Route exact path="/" element={<Home />} />
-      <Route exact path="/header" element={<Header />} />
+      <Route exact path="/header" element={<Navigation />} />
       <Route exact path="/search" element={<Search />} />
     </Routes>
   </>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,28 +6,28 @@ import cart from '../images/online-shopping.png';
 import bell from '../images/bell.png';
 import user from '../images/user.png';
 
-const Header = () => (
-  <header className="flex flex-wrap items-center justify-between h-16 p-4 md:p-8">
+const Navigation = () => (
+  <nav className="flex flex-wrap items-center justify-between h-16 p-4 md:p-8">
     <div className="">
       <img src={logo} alt="Logo" className="h-[12px] md:h-[20px] lg:h-[20px] w-14 md:w-[100px]" />
     </div>
     <Search />
     <div className="flex items-center space-x-4 ">
       <span className="hidden md:flex">
-        <img src={cart} alt="menu" className="w-8 p-2 border rounded-full" />
+        <img src={cart} alt="menu" className="w-10 p-2 border border-yellow-500 rounded-full" />
       </span>
       <span className="hidden md:flex">
-        <img src={bell} alt="menu" className="w-8 p-2 border rounded-full" />
+        <img src={bell} alt="menu" className="w-10 p-2 border border-yellow-500 rounded-full" />
       </span>
       <span className="hidden md:flex">
-        <img src={user} alt="menu" className="w-8 p-2 border rounded-full" />
+        <img src={user} alt="menu" className="w-10 p-2 border border-yellow-500 rounded-full" />
       </span>
     </div>
     <span className="justify-center sm:flex md:hidden">
       <img src={menu} alt="menu" className="w-8 p-2 border rounded-full" />
     </span>
-  </header>
+  </nav>
 
 );
 
-export default Header;
+export default Navigation;
