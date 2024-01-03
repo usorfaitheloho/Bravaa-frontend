49b7609cec5b55baa002c8995286d96e98165b1c
usorfaitheloho <usorfaitheloho@gmail.com>
2024-01-03 23:23:39 +0200

    Update the search component for desktop view

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,14 +2,14 @@ import React from 'react';
 import search from '../images/search.png';
 
 const Search = () => (
-  <div className="relative flex items-center">
+  <div className="relative flex items-center justify-center mx-4">
     <input
       type="text"
-      placeholder="Searching..."
-      className="w-56 p-1 pl-8 placeholder-green-700 border border-gray-300 rounded-full pl pl- pl-8pr-12 pr- w- focus:outline-none focus:border-blue-500 lg:w-2/5 placeholder:text-sm"
+      placeholder="Search..."
+      className="w-56 p-1 pl-10 placeholder-green-700 border border-gray-300 rounded-full focus:outline-none focus:border-blue-500 md:w-[40rem] md:h-[2.5rem] placeholder:text-sm"
     />
-    <div className="absolute text-gray-500 left-2 top-2">
-      <img src={search} className="md:h-[20px] p-1" alt="search" />
+    <div className="absolute text-gray-500 left-4 top-3">
+      <img src={search} className="md:h-[20px] p-0.5" alt="search" />
     </div>
   </div>
 );
